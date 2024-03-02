e3f9de6d19d0b5237130814ae278b591f1542b90
Arnav Aggarwal <arnavaggarwal2012@gmail.com>
2024-03-02 20:49:20 +0530

    only portait mode

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,12 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:kabotr/features/onboarding/ui/onboarding_screen.dart';
 import 'package:kabotr/firebase_options.dart';
 import 'package:kabotr/themes/app_theme.dart';
 
 void main() async {
-  WidgetsFlutterBinding.ensureInitialized;
+  WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform
   );
@@ -17,6 +18,10 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+     SystemChrome.setPreferredOrientations([
+      DeviceOrientation.portraitUp,
+      DeviceOrientation.portraitDown,
+    ]);
     return MaterialApp(
       theme: Apptheme.darkTheme,
       home: const OnboardingScreen(),
