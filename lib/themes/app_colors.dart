485e63fe98fb051fbcedef23241222191c17a762
Arnav Aggarwal <arnavaggarwal2012@gmail.com>
2024-03-02 20:47:14 +0530

    updated themes

---
+++ lib/themes/app_colors.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+class AppColor{
+  static const Color purple =  Color.fromARGB(255, 160, 128, 215);
+  static Color greyTextField =Colors.grey.shade200;
+}
--- lib/themes/app_images.dart
+++ lib/themes/app_images.dart
@@ -2,4 +2,5 @@ class AppImages {
   static const String kingqueen = 'assets/kingqueen.png';
   static const String logo = 'assets/kobotr-logo-square.png';
   static const String letter = 'assets/letter.png';
+  static const String logoTextWhite = 'assets/kabotr-text-white.png';
 }
